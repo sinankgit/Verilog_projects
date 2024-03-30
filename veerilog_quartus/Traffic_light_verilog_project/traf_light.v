4c1966f659be34c3c58c0ce15012136f6ffffa05
Muhammed Sinan <91724766+sinankgit@users.noreply.github.com>
2024-03-30 21:09:12 +0530

    Update traf_light.v

--- veerilog_quartus/Traffic_light_verilog_project/traf_light.v
+++ veerilog_quartus/Traffic_light_verilog_project/traf_light.v
@@ -49,7 +49,7 @@ always @(posedge sec_clock, posedge reset) begin
 	// on reset it goes to north green light
 	if (reset)
 		present_state <= North_green;
-	// if the next state is yellow then we need to wait for five seconds
+	// if the next state is yellow then we need to wait for five seconds in the green light
 	else if (next_yellow) begin
 		if (reached_five)
 			present_state <= next_state;
